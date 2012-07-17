c11cd044e62340967e3c4a8d6d6ba0a91a901d41
Anand Ubale <anandu@gmail.com>
2012-07-17 09:32:12 +0000

    metadata updated w receipe

--- cookbooks/app_django/metadata.rb
+++ cookbooks/app_django/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures app_django"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe  "app_django::default", "Installs the Django application server."
